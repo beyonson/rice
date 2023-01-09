f2510a5746af315916d9efff0b3fda3e7632acb5
beyonson <garrettmarcinak@gmail.com>
2023-01-09 00:27:45 -0500

    change to light theme

--- polybar/workspaces.py
+++ polybar/workspaces.py
@@ -13,10 +13,10 @@ else:
     for ws in workspaces:
         if ws.num == 1:
             exists = True
-            out += "" + " "
+            out += "%{F#ffffff}" + "" + " %{F-}"
             break
     if not exists:
-        out += "" + " "
+        out += "%{F#ffffff}" +"" + " %{F-}"
 
 if 2 == focused:
     out += "%{F#ffffff}" + "" + " %{F-}"
@@ -25,10 +25,10 @@ else:
     for ws in workspaces:
         if ws.num == 2:
             exists = True
-            out += "" + " "
+            out += "%{F#ffffff}" + "" + " %{F-}"
             break
     if not exists:
-        out += "" + " "
+        out += "%{F#ffffff}" +"" + " %{F-}"
 
 if 3 == focused:
     out += "%{F#ffffff}" + "" + " %{F-}"
@@ -37,10 +37,10 @@ else:
     for ws in workspaces:
         if ws.num == 3:
             exists = True
-            out += "" + " "
+            out += "%{F#ffffff}" + " " + "%{F-}"
             break
     if not exists:
-        out += "" + " "
+        out += "%{F#ffffff}" +" " + "%{F-}"
 
 if 4 == focused:
     out += "%{F#ffffff}" + "" + " %{F-}"
@@ -49,70 +49,21 @@ else:
     for ws in workspaces:
         if ws.num == 4:
             exists = True
-            out += "" + " "
+            out += "%{F#ffffff}" + " " + "%{F-}"
             break
     if not exists:
-        out += "" + " "
+        out += "%{F#ffffff}" +" " +  "%{F-}"
 
 if 5 == focused:
-    out += "%{F#ffffff}" + "" + " %{F-}"
+    out += "%{F#ffffff}" + "" + "%{F-}"
 else:
     exists = False
     for ws in workspaces:
         if ws.num == 5:
             exists = True
-            out += "" + " "
+            out += "%{F#ffffff}" + "" + "%{F-}"
             break
     if not exists:
-        out += "" + " "
-
-if 6 == focused:
-    out += "%{F#ffffff}" + "" + " %{F-}"
-else:
-    exists = False
-    for ws in workspaces:
-        if ws.num == 6:
-            exists = True
-            out += "" + " "
-            break
-    if not exists:
-        out += "" + " "
-
-if 7 == focused:
-    out += "%{F#ffffff}" + "" + " %{F-}"
-else:
-    exists = False
-    for ws in workspaces:
-        if ws.num == 7:
-            exists = True
-            out += "" + " "
-            break
-    if not exists:
-        out += "" + " "
-
-if 8 == focused:
-    out += "%{F#ffffff}" + "" + " %{F-}"
-else:
-    exists = False
-    for ws in workspaces:
-        if ws.num == 8:
-            exists = True
-            out += "" + " "
-            break
-    if not exists:
-        out += "" + " "
-
-if 9 == focused:
-    out += "%{F#ffffff}" + "" + "%{F-}"
-else:
-    exists = False
-    for ws in workspaces:
-        if ws.num == 9:
-            exists = True
-            out += ""
-            break
-    if not exists:
-        out += ""
-
+        out += "%{F#ffffff}" + "" + "%{F-}"
 
 print(out)
